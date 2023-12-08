d4c47f8d41d5f29ca843927394ecd7a8c851a2e0
Gabriel Carvalho <113658845+Gabriel281065@users.noreply.github.com>
2023-12-08 19:46:55 -0300

    teste

--- test/somar.ts
+++
@@ -1,2 +0,0 @@
-export const somar = (a: number, b: number) => a + b;
-
--- test/user.service.spec.ts
+++ test/user.service.spec.ts
@@ -1,11 +1,8 @@
-import { somar } from "../test/somar";
+import {somar} from "src/utils/somar"; 
 
 test('sera um teste:', () => {
 
     const resultado = somar(3,2);
-
     expect(resultado).toEqual(5); 
-
-   
 
 });
